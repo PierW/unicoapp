e2c6a724208edec5d685f104381fc6ad240ae1d4
Pierpaolo Wurzburger <pierpaolo.wurzburger@meetweb.it>
2025-09-15 16:24:55 +0200

    inserisco aggiornamento versione tramite service worker con messaggio banner

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
+const CACHE_NAME = "static-v1.0.3"; // Cambia versione ad ogni release per mostrare messaggio banner per aggiornare la PWA
+
 self.addEventListener("install", e =>{
     e.waitUntil(
-        caches.open("static").then(cache => {
+        caches.open(CACHE_NAME).then(cache => {
             return cache.addAll([
                 "./",
                 "./style.css",
-                "./apple-touch-icon.png",
+                "./apple-touch-icon.webp",
                 "./images/left-arrow.png",
                 "./images/home.png",
                 "./images/crypt.png",
@@ -19,10 +21,29 @@ self.addEventListener("install", e =>{
     );
 });
 
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
 self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(response => {
             return response || fetch(e.request);
         })
     )
+});
+
+// Notifica aggiornamento (da gestire nel client)
+self.addEventListener("message", event => {
+    if (event.data === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
 });
